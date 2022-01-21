7e01f4bfaac76f33d416d57009121252c318557a
andreaht <pasetti.andrea@gmail.com>
2022-01-21 09:33:10 +0100

    Added FakeLocalDataSource.kt

---
+++ app/src/sharedTest/java/com/example/github/igenius/githubrepositories/data/local/FakeLocalDataSource.kt
@@ -0,0 +1,30 @@
+package com.example.github.igenius.githubrepositories.data.local
+
+import com.example.github.igenius.githubrepositories.data.dto.RepositoryDTO
+import com.example.github.igenius.githubrepositories.data.dto.Result
+
+class FakeLocalDataSource : ILocalDataSource {
+
+    private val repositories = mutableListOf<RepositoryDTO>()
+
+    override suspend fun getRepos(): Result<List<RepositoryDTO>> {
+        return Result.Success(repositories)
+    }
+
+    override suspend fun saveRepo(repository: RepositoryDTO) {
+        repositories.add(repository)
+    }
+
+    override suspend fun getRepo(name: String): Result<RepositoryDTO> {
+        val repository = repositories.filter { it.name == name }
+        return if (repository.isNotEmpty()) {
+            Result.Success(repository.first())
+        } else {
+            Result.Error("Repository not found!")
+        }
+    }
+
+    override suspend fun deleteAllRepos() {
+        repositories.clear()
+    }
+}
